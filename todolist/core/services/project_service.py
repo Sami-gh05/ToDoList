db9c9be2573f8a15c40cde9bf91a4bea2d719816
Sam Ghorbani <Sam.ghorbani8490@gmail.com>
2025-10-19 02:33:15 +0330

    fix: improve validations in edit methods

--- todolist/core/services/project_service.py
+++ todolist/core/services/project_service.py
@@ -20,19 +20,16 @@ class ProjectService:
     """
     
     project_repo: ProjectRepository
-    task_repo = TaskRepository
-    settings = Settings
+    task_rep: TaskRepository
+    settings: Settings
     
     def creat_project(self, name: str, description: str = "") -> Project:
         if self.project_repo.get_by_name(name) is not None:
             raise ValueError("Project name must be unique.")
-        
         existing_count: int = len(list(self.project_repo.list_all_projects))
         if existing_count == self.settings.MAX_PROJECTS:
             raise ValueError("You have reached maximum number of projects.")
-        
         project = Project(id = self.project_repo.next_available_id(), name = name, description = description)
-        
         return self.project_repo.add(project)
     
     def delete_project(self, project_identifier: Union[int, str]) -> bool:
@@ -41,10 +38,8 @@ class ProjectService:
             project = self.project_repo.get_by_id(project_identifier)
         elif isinstance(project_identifier, str):
             project = self.project_repo.get_by_name(project_identifier)
-            
         if project is None:
             return False
-        
         # Cascade deleting tasks
         self.task_repo.remove_by_project(project.id)
         return self.project_repo.remove(project.id)
@@ -61,28 +56,25 @@ class UpdateProject:
     
     def edit_project_name(self, project_id: int, *, name: str) -> Project:
         project: Project = self.project_repo.get_by_id(project_id)
-    
         if project is None:
             raise ValueError("Project not found.")
-        
+        if not name or len(name.strip()) == 0:
+            raise ValueError("Name of project cannot be empty")
         if name != project.name:
             if self.project_repo.get_by_name(name) is not None:
                 raise ValueError("Project name must be unique.")
-            
-        tempProject = Project(project.name, project.description)
-        tempProject.name = name
-        tempProject.validate()
-        return self.project_repo.update(tempProject)
+        if len(name) > Settings.MAX_NAME_LEN:
+            raise ValueError(f"Length of project name cannot be more than {Settings.MAX_NAME_LEN} characters.")
+        project.name = name
+        return self.project_repo.update(project)
     
     def edit_project_description(self, project_id: int,  *, description: str) -> Project:
         project: Project = self.project_repo.get_by_id(project_id)
-        
         if project is None:
             raise ValueError("Project not found.")
-        
-        tempProject = Project(project.name, project.description)
-        tempProject.description = description
-        tempProject.validate()
-        return self.project_repo.update(tempProject)
+        if len(description) > Settings.MAX_DESCRIPTION_LEN:
+            raise ValueError(f"Length of project description cannot be more than {Settings.MAX_DESCRIPTION_LEN} characters.")
+        project.description = description
+        return self.project_repo.update(Project)
     
     
