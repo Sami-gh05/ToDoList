0eb06ea47a3deac54e35e6fd7397474e3d51f811
Sam Ghorbani <Sam.ghorbani8490@gmail.com>
2025-12-09 00:57:25 +0330

    Refactor: Change Repository layer implementation for Project entity in order to use sqlalchemy

---
+++ todolist/data/db/project_model.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from todolist.data.db.sql_db_base import Base
+
+from todolist.config.settings import Settings
+
+class ProjectModel(Base):
+    """SQLAlchemy model for Project entity"""
+    __tablename__ = "projects"
+    
+    settings = Settings.load()
+    id = Column(Integer, primary_key=True)
+    name = Column(String(settings.MAX_NAME_LEN), nullable=False)
+    description = Column(String(settings.MAX_DESCRIPTION_LEN), nullable=True)
+
+    tasks = relationship("TaskModel", back_populates="project")
+
+    def __repr__(self):
+        return f"Project(id={self.id}, name={self.name}, description={self.description})"
+
+    def __str__(self):
+        return f"Project(id={self.id}, name={self.name}, description={self.description})"
+
+    def __eq__(self, other):
+        return self.id == other.id
